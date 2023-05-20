30705f14556940db63e8c2f9b691d66331ce96bf
cksghkd123 <cksghkd1234@naver.com>
2023-05-20 14:42:11 +0900

    first commit!

---
+++ src/test/java/com/BDFH/fakeGG/FakeGgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BDFH.fakeGG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeGgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
