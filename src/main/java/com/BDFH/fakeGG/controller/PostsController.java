00d1b426c1613c2e161bb39f80350eb5f191e46c
cksghkd123 <cksghkd1234@naver.com>
2023-05-21 00:02:56 +0900

    feat:기본틀

---
+++ src/main/java/com/BDFH/fakeGG/controller/PostsController.java
@@ -0,0 +1,10 @@
+package com.BDFH.fakeGG.controller;
+
+
+import com.BDFH.fakeGG.entity.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostsController {
+}
