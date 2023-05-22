c5d242822d4efa2950e669eceee3b0867586822f
Hwan0518 <qip0518@naver.com>
2023-05-22 15:16:56 +0900

    feat: 게시글 목록 기능 개발(2)
    postsList - Pageable을 파라미터로 받아서, 페이징 처리 완료

--- src/main/java/com/BDFH/fakeGG/repository/UserRepository.java
+++ src/main/java/com/BDFH/fakeGG/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.BDFH.fakeGG.repository;
 
 import com.BDFH.fakeGG.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
