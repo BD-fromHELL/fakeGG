8e33937d9a0851cfe3102a4f405543e126bfa65f
HwangYoungJun <dudghkd4512@gmail.com>
2023-05-23 21:26:52 +0900

    feat: Posts => Article로 리펙터링

--- src/main/java/com/BDFH/fakeGG/service/ArticleService.java
+++ src/main/java/com/BDFH/fakeGG/service/ArticleService.java
@@ -1,10 +1,10 @@
 package com.BDFH.fakeGG.service;
 
-import com.BDFH.fakeGG.dto.PostsResponseDTO;
-import com.BDFH.fakeGG.dto.PostPostsRequestDTO;
-import com.BDFH.fakeGG.entity.Posts;
+import com.BDFH.fakeGG.dto.ArticleResponseDTO;
+import com.BDFH.fakeGG.dto.PostArticleRequestDTO;
+import com.BDFH.fakeGG.entity.Article;
 import com.BDFH.fakeGG.exception.NotFoundPostsException;
-import com.BDFH.fakeGG.repository.PostsRepository;
+import com.BDFH.fakeGG.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,17 +17,17 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class PostsService {
-    private final PostsRepository postsRepository;
+public class ArticleService {
+    private final ArticleRepository articleRepository;
 
     /**
      * 게시글 목록 : 전체 게시글을 10개씩 끊어서 return. List로 return
      */
     @Transactional(readOnly = true)
-    public List<PostsResponseDTO> findAll(Pageable pageable){
-        Page<Posts> entity = postsRepository.findAll(pageable);
+    public List<ArticleResponseDTO> findAll(Pageable pageable) {
+        Page<Article> entity = articleRepository.findAll(pageable);
         return entity.stream()
-                .map(PostsResponseDTO::new)
+                .map(ArticleResponseDTO::new)
                 .collect(Collectors.toList());
     }
 
@@ -35,22 +35,21 @@ public class PostsService {
      * 게시글 작성
      */
     @Transactional
-    public Posts postPosts(PostPostsRequestDTO request){
-        Posts posts = Posts.builder()
+    public Article postArticle(PostArticleRequestDTO request) {
+        Article article = Article.builder()
                 .title(request.getTitle())
                 .contents(request.getContents())
                 .build();
-        return postsRepository.save(posts);
+        return articleRepository.save(article);
     }
 
-
     /**
-     * 게시글 삭제 : postsId에 해당하는 게시글을 삭제한다
+     * 게시글 삭제 : articleId에 해당하는 게시글을 삭제한다
      */
     @Transactional
-    public void deletePosts(Long postsId){
-        Posts posts = postsRepository.findById(postsId)
+    public void deleteArticle(Long articleId) {
+        Article article = articleRepository.findById(articleId)
                 .orElseThrow(() -> new NotFoundPostsException("게시글이 존재하지 않습니다"));
-        postsRepository.delete(posts);
+        articleRepository.delete(article);
     }
 }
